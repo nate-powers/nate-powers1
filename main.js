55f2aff7eba5f9580d246be6f1980524cadc40fa
nate-powers <73077132+nate-powers@users.noreply.github.com>
2024-05-10 09:36:02 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,55 @@
 <script>
 
-function button.onMouseClick {
-	document.getElementById(#button);
+function button.OnMouseDown() {
+	document.getElementById("#button");
 	alert("Welcome to my website");
+}
+function button.OnMouseDown() {
+	document.getElementById("#button");
+	window.alert(Math.random(50);
+}
+Object {
+    Button.constructor();
+    getElementsByTagName('setTimeout(function() {}, 10);')
+
+}
+getTime()
+Date(MM/DD/YYYY)
+
+var button {}
+alert("Welcome to my website");
+function button {
+	window.alert("Welcome to my website");
+}
+function   {
+}
+
+
+	function button.OnMouseDown() {
+	document.getElementById("button");
+	alert("Welcome to my website!")
 }
 
 
 
+array("hammerhead", "whale", "penguins");
+
+	var Animals = Things.length
+	function (button) {
+		for (var i = Things.length - 1; i >= 0; i--) {
+			Things[i]
+		}
+		=if (true) {
+button.prototype.constructor = function(button) {
+    alert("Welcome to my website")
+};
+} else {
+
+}
+	Object {
+      Animal()
+	}
+
+	OnMouseDown.getDate();
+	}
 </script>
